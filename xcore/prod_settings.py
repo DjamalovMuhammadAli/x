264de12b74b9328697d851d79c1911a23b02a726
DjamalovMuhammadAli <djamalov.muhammad.ali7@gmail.com>
2022-12-23 10:45:46 +0500

    .

---
+++ xcore/prod_settings.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from .settings import BASE_DIR
+
+SECRET_KEY = 'django-insecure-*7rcnv=abdan&szjd#$k*ipg*u6yrju=hi2p2599lwvpl-j!*_'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1', 'domein', 'www.domain.uz']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'x_db',
+        'USER': 'postgres',
+        'PASSWORD': 'ali',
+        'HOST': 'localhost',
+        'port': '5432',
+    }
+}
